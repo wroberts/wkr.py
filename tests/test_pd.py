d7283882ef38efe42b809bde7b556ea04b083f95
Will Roberts <will.roberts@rz.hu-berlin.de>
2017-06-27 12:59:16 +0200

    tests/test_pd.py: test memoize_pandas loads from CSV

--- tests/test_pd.py
+++ tests/test_pd.py
@@ -24,8 +24,8 @@ def dataframe_gen():
                         {'a': 5, 'b': 10, 'c': 20}])
 
 
-def test_memoize_pandas(tmpdir):
-    """Test `wkr.pd.memoize_pandas`."""
+def test_memoize_pandas_save(tmpdir):
+    """Test that `wkr.pd.memoize_pandas` saves to CSV."""
     for idx, df in enumerate(dataframe_gen()):
         filename = tmpdir.join('data{}.csv'.format(idx))
 
@@ -40,3 +40,29 @@ def test_memoize_pandas(tmpdir):
         df3 = pd.read_csv(filename.strpath,
                           encoding='utf-8', index_col=0)
         assert df3.equals(df)
+
+
+def test_memoize_pandas_load(tmpdir):
+    """Test that `wkr.pd.memoize_pandas` loads from CSV."""
+    for idx, df in enumerate(dataframe_gen()):
+        filename = tmpdir.join('data{}.csv'.format(idx))
+
+        # define a memoized function that returns a known value
+        @pandas_memoize(filename.strpath)
+        def f():
+            return df
+
+        # now write to its CSV file a value that is different
+        df2 = pd.DataFrame({'x': range(40, 30, -1), 'y': range(20, 30)},
+                           index=range(5, 15))
+        assert not df2.equals(df)
+        df2.to_csv(filename.strpath, encoding='utf-8')
+        assert filename.exists()
+
+        # show that f() now returns df2, not df
+        df3 = f()
+        assert not df3.equals(df)
+        assert df3.equals(df2)
+
+        # remove the CSV file
+        filename.remove()
