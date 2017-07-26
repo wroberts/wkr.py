1154d509f874f9a7477fb3b091826166ef26bc20
Will Roberts <will.roberts@rz.hu-berlin.de>
2017-07-26 19:25:57 +0200

    wkr/os.py: atomic file i/o with backups

--- wkr/os.py
+++ wkr/os.py
@@ -3,6 +3,10 @@
 """
 OS-level routines.
 
+Parts of the atomic file I/O are adapted from the code given here:
+
+http://stackoverflow.com/a/29491523/1062499
+
 os.py
 (c) Will Roberts  23 June, 2017
 """
@@ -11,6 +15,11 @@ from __future__ import absolute_import
 
 import errno
 import os
+import shutil
+import tempfile
+from contextlib import contextmanager
+
+import wkr.io
 
 
 def mkdir_p(path):
@@ -22,3 +31,81 @@ def mkdir_p(path):
             pass
         else:
             raise
+
+
+@contextmanager
+def temp_file_name(suffix='', directory=None):
+    """
+    Context manager for creating temporary file names.
+
+    Will find a free temporary filename upon entering and will try to
+    delete the file on leaving, even in case of an exception.
+
+    Arguments:
+    :param str suffix: optional file suffix
+    :param str directory: optional directory to save temporary file in
+    """
+    tfile = tempfile.NamedTemporaryFile(delete=False,
+                                        suffix=suffix,
+                                        dir=directory)
+    tfile.file.close()
+    try:
+        yield tfile.name
+    finally:
+        try:
+            os.remove(tfile.name)
+        except OSError as err:
+            if err.errno == errno.ENOENT:  # No such file or directory
+                pass
+            else:
+                raise
+
+
+@contextmanager
+def open_atomic(filepath, mode='w+b', fsync=False, **kwargs):
+    """
+    Open atomic temporary file object.
+
+    The returned file atomically moves to destination upon exiting.
+
+    Allows reading and writing to and from the same filename.
+
+    The file will not be moved to destination in case of an exception.
+
+    :param int filepath: the file path to be opened
+    :param int fsync: whether to force write the file to disk
+    :param int **kwargs: Any valid keyword arguments for `open`
+    """
+    with temp_file_name(
+            directory=os.path.dirname(os.path.abspath(filepath))) as tmppath:
+        with wkr.io.open_file(tmppath, mode, **kwargs) as output_file:
+            try:
+                yield output_file
+            finally:
+                if fsync:
+                    output_file.flush()
+                    os.fsync(output_file.fileno())
+        os.rename(tmppath, filepath)
+
+
+def backup_file(filename):
+    """Back up the old file, if it exists."""
+    if os.path.exists(filename):
+        if os.path.exists(filename + '~'):
+            os.unlink(filename + '~')
+        shutil.copyfile(filename, filename + '~')
+
+
+def write_atomic(lines, output_filename, backup=True):
+    """
+    Write the given chunks to the named file atomically.
+
+    :param iterable lines:
+    :param str output_filename:
+    :param bool backup: Defaults to True
+    """
+    if backup:
+        backup_file(output_filename)
+    with open_atomic(output_filename, 'wb') as output_file:
+        for line in lines:
+            output_file.write(line)
