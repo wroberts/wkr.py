f9f46bf34f5a35528d5cbcac2c0da31bf03caba8
Will Roberts <will.roberts@rz.hu-berlin.de>
2019-11-25 18:12:02 +0100

    tests/test_wkr.py fix failing test

--- tests/test_wkr.py
+++ tests/test_wkr.py
@@ -197,7 +197,7 @@ def test_chunks():
     assert list(wkr.chunks(range(10), 3)) == [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9]]
     assert list(wkr.chunks(range(10), 4)) == [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9]]
     with pytest.raises(ValueError):
-        wkr.chunks(range(10), 0)
+        list(wkr.chunks(range(10), 0))
 
 
 def test_pairwise():
